6395941c1536e351b6242eb53947e576d95cd307
Arnauw <arnaud.rabel@gmail.com>
2025-11-12 21:38:18 +0100

    API done?

--- frontend/lib/API.ts
+++ frontend/lib/API.ts
@@ -1,2 +1,16 @@
+import {Product} from "@/type";
+
 const API_URL = "https://fakestoreapi.com/products";
 
+const getProducts = async (): Promise<Product[]> => {
+    try {
+        const response = await fetch(`${API_URL}/products`);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching products", error);
+        throw error;
+    }
+}
